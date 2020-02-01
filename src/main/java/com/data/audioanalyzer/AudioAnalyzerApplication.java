09096429fd4f96aec79e631c0e26663522975d49
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-02-01 14:14:42 +0530

    initial commit

---
+++ src/main/java/com/data/audioanalyzer/AudioAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.data.audioanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/audioanalyzer/AudioAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.audioanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
